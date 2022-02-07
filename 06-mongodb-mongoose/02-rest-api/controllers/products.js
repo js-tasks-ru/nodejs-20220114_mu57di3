1e5e6947eef76ab78397634079d39c605ec9af69
Bender <mu57di3@gmail.com>
2022-02-07 16:27:23 +0300

    add solution for 6-module 2-task

--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,16 +1,46 @@
+const Product = require('../models/Product');
+const mapProduct = require('../mappers/product');
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
   const {subcategory} = ctx.query;
 
   if (!subcategory) return next();
 
-  ctx.body = {};
+  const list = await Product.find({subcategory: subcategory}).populate('category');
+
+  ctx.body = {
+    products: list.map(mapProduct),
+  };
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const list = await Product.find({}).populate('category');
+
+  ctx.body = {
+    products: list.map(mapProduct),
+  };
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  const {id} = ctx.params;
+  let product;
+
+  if (!id) {
+    ctx.throw(404);
+  }
+
+  try {
+    product = await Product.findOne({_id: id});
+  } catch (error) {
+    ctx.throw(400);
+  }
+
+  if (!product) {
+    ctx.throw(404);
+  }
+
+  ctx.body = {
+    product: mapProduct(product),
+  };
 };
 
