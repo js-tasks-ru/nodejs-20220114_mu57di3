b05440bcc1f5771cb0c81dc51a27ef647d13273f
Bender <mu57di3@gmail.com>
2022-02-10 17:22:46 +0300

    Облажался забыл return добавить

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,11 +1,10 @@
 const User = require('../../models/User');
-const mongoose = require('mongoose');
 
 module.exports = async function authenticate(strategy, email, displayName, done) {
   let userData;
 
   if (!email) {
-    done(null, false, 'Не указан email');
+    return done(null, false, 'Не указан email');
   }
 
   userData = await User.findOne({email: email});
